b2a39a1cffc083a915945b255019e561a83ff7b1
divya20m <137371387+divya20m@users.noreply.github.com>
2023-07-18 20:11:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+   
+   setTimeout(function() {
+    document.getElementById('timer').innerText = 10;
+  
+    setTimeout(function() {
+      document.getElementById('timer').innerText = 9;
+  
+      setTimeout(function() {
+        document.getElementById('timer').innerText = 8;
+  
+        setTimeout(function() {
+          document.getElementById('timer').innerText = 7;
+  
+          setTimeout(function() {
+            document.getElementById('timer').innerText = 6;
+  
+            setTimeout(function() {
+              document.getElementById('timer').innerText = 5;
+  
+              setTimeout(function() {
+                document.getElementById('timer').innerText = 4;
+  
+                setTimeout(function() {
+                  document.getElementById('timer').innerText = 3;
+  
+                  setTimeout(function() {
+                    document.getElementById('timer').innerText = 2;
+  
+                    setTimeout(function() {
+                      document.getElementById('timer').innerText = 1;
+                      document.getElementById('timer').innerText = "Happy Independence Day!";
+                    }, 1000);
+  
+                  }, 1000);
+  
+                }, 1000);
+  
+              }, 1000);
+  
+            }, 1000);
+  
+          }, 1000);
+  
+        }, 1000);
+  
+      }, 1000);
+  
+    }, 1000);
+  
+  }, 1000);
+  
